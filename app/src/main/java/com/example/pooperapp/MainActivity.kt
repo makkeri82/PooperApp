aeb48d4f0f5667ab0f6d09e9344c7a65e793a561
Marko <marko.peitsila@gmail.com>
2025-10-22 14:03:35 +0300

    Added PooperButton to main view

--- app/src/main/java/com/example/pooperapp/MainActivity.kt
+++ app/src/main/java/com/example/pooperapp/MainActivity.kt
@@ -2,9 +2,11 @@
 
 package com.example.pooperapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
@@ -13,6 +15,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.pooperapp.ui.theme.PooperAppTheme
 import com.google.android.gms.maps.model.LatLng
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MyLocation
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,18 +37,24 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@SuppressLint("SuspiciousIndentation")
 @Composable
 fun MyApp(modifier: Modifier = Modifier) {
     val ouluLocation = LatLng(65.0121, 25.4651)
-    val poopData = PoopData()
 
-    Scaffold { innerPadding ->
-
+    Box(
+        modifier = modifier.fillMaxSize()
+    ) {
         Maps(
-            modifier = Modifier.padding(innerPadding),
+            modifier = Modifier.fillMaxSize(),
             location = ouluLocation,
-            poopData = poopData.getPoopData()
-            //mapProps = props
+            poopData = PoopData().getPoopData()
+        )
+        PooperButton(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(16.dp),
+            onClick = {}
         )
     }
 }
---
+++ app/src/main/java/com/example/pooperapp/PooperButton.kt
@@ -0,0 +1,39 @@
+package com.example.pooperapp
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import java.nio.file.WatchEvent
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun PooperButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit
+) {
+    FloatingActionButton(
+        onClick = onClick,
+        modifier = modifier
+            .clip(CircleShape)
+    ) {
+        Image(
+            painter = painterResource(R.drawable.poop_img),
+            contentDescription = null
+
+        )
+        Text(
+            text ="+",
+            fontSize = 26.sp,
+            color = Color.White
+        )
+    }
+}
